2ccbe035a5c88cc9871413eaeca536624a3cc5b5
MooseMan <ssaciolo@gmail.com>
2020-01-14 11:40:06 -0500

    Polished UI

--- src/components/TodoComponents/TodoForm.js
+++ src/components/TodoComponents/TodoForm.js
@@ -1,20 +1,28 @@
 import React from "react"
-import Input from "@material-ui/core/Input"
+import TextField from "@material-ui/core/TextField"
 import Button from "@material-ui/core/Button"
 
 const TodoForm = props => {
   return (
     <div>
       <form onSubmit={props.handleSubmit} noValidate autoComplete="off">
-        <Input
+        <TextField
           type="text"
           name="item"
           placeholder="Todo..."
           value={props.toDoText}
           onChange={props.handleChanges}
+          id="standard-basic"
+          label="Add New ToDo"
+          variant="standard"
         />
         &nbsp;
-        <Button type="submit" variant="contained" color="primary">
+        <Button
+          className="add-butt"
+          type="submit"
+          variant="contained"
+          color="primary"
+        >
           Add Todo
         </Button>
       </form>
